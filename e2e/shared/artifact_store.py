8901921acf1eaba93a313e8f181a181f69392331
Abhishek Aggarwal <aaggarwal@mozilla.com>
2024-04-12 11:15:28 +0200

    Replace artifact_store.py from Dexterp37/mlops-codecopilot-demo
     - Copied from
       https://github.com/Dexterp37/mlops-codecopilot-demo/blob/414aa84982c782238dc7c224b55cdcea27c67ad8/artifact_store.py

--- e2e/shared/artifact_store.py
+++ e2e/shared/artifact_store.py
@@ -7,18 +7,20 @@ from pathlib import Path
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-PROJECT_NAME = "moz-fx-mlops-inference-nonprod"
-BUCKET_NAME = "mf-models-test1"
 
 class ArtifactStore:
-    def _get_storage_path(self,flow_name: str, run_id: str, file_name: str) -> str:
+    def __init__(self, project_name: str, bucket_name: str):
+        self.gcs_project_name = project_name
+        self.gcs_bucket_name = bucket_name
+
+    def _get_storage_path(self, flow_name: str, run_id: str, file_name: str) -> str:
         return f"{flow_name}/{run_id}/{file_name}"
 
-    def _internal_store(self, data: bytes, storage_path: str) -> str:
+    def store(self, data: bytes, storage_path: str) -> str:
         from google.cloud import storage
 
-        client = storage.Client(project=PROJECT_NAME)
-        bucket = client.get_bucket(BUCKET_NAME)
+        client = storage.Client(project=self.gcs_project_name)
+        bucket = client.get_bucket(self.gcs_bucket_name)
 
         blob = bucket.blob(storage_path)
 
@@ -31,33 +33,38 @@ class ArtifactStore:
             blob.upload_from_file(f, if_generation_match=0)
             logging.info(f"The model is stored at {storage_path}")
 
-    def store(self, data: bytes, filename: str) -> str:
+    def fetch(self, remote_path: str, local_path: str) -> str:
+        from google.cloud import storage
+
+        client = storage.Client(project=self.gcs_project_name)
+        bucket = client.get_bucket(self.gcs_bucket_name)
+
+        blob = bucket.blob(remote_path)
+
+        # Create any directory that's needed.
+        p = Path(local_path)
+        p.parent.mkdir(parents=True, exist_ok=True)
+
+        blob.download_to_filename(local_path)
+
+    def store_flow_data(self, data: bytes, filename: str) -> str:
         from metaflow import current
-        
-        self.deployment_path = self._get_storage_path(
+
+        deployment_path = self._get_storage_path(
             current.flow_name, current.run_id, filename
         )
 
-        self._internal_store(data, self.deployment_path)
+        self.store(data, deployment_path)
 
-        return self.deployment_path
+        return deployment_path
 
-    def fetch(self, flow_name: str, run_id: str, file_name: str) -> str:
+    def fetch_flow_data(self, flow_name: str, run_id: str, file_name: str) -> str:
         from google.cloud import storage
 
         path = self._get_storage_path(
             flow_name=flow_name, run_id=run_id, file_name=file_name
         )
 
-        client = storage.Client(project=PROJECT_NAME)
-        bucket = client.get_bucket(BUCKET_NAME)
-
-        blob = bucket.blob(path)
-
-        # Create any directory that's needed.
-        p = Path(path)
-        p.parent.mkdir(parents=True, exist_ok=True)
-
-        blob.download_to_filename(path)
+        self.fetch(remote_path=path, local_path=path)
 
         return path
