28526c5e5b8946ea1ea564132c56bdfb3810f0e1
Abhishek Aggarwal <aaggarwal@mozilla.com>
2024-04-15 10:18:48 +0200

    Added a function to fetch an entire sub-directory within a gcs bucket

--- e2e/artifact_store.py
+++ e2e/artifact_store.py
@@ -16,6 +16,10 @@ class ArtifactStore:
     def _get_storage_path(self, flow_name: str, run_id: str, file_name: str) -> str:
         return f"{flow_name}/{run_id}/{file_name}"
 
+    def _get_flow_storage_directory(self, flow_name: str, run_id: str, remote_subdir: str) -> str:
+        #ToDo: use os.path.join instead
+        return f"{flow_name}/{run_id}/{remote_subdir}"
+
     def store(self, data: bytes, storage_path: str) -> str:
         from google.cloud import storage
 
@@ -47,6 +51,30 @@ class ArtifactStore:
 
         blob.download_to_filename(local_path)
 
+    def fetch_directory(self, remote_directory: str, local_directory: str) -> str:
+        """Download all blobs from a directory within a gcs bucket to a directory on local machine.
+
+        The filename of each blob once downloaded to the `local_directory` is derived from the blob name
+        within the `remote_directory` (i.e. relative blob name wrt to the `remote_directory` parameter).
+        This means, the full path of the each downloaded blob on local machine can be computed by replacing
+        `remote_directory` prefix in the blob name with `local_directory`.
+
+        Directories will be created automatically as needed to accommodate blob
+        names that include slashes."""
+
+        from google.cloud import storage
+        from google.cloud.storage import transfer_manager
+
+        client = storage.Client(project=self.gcs_project_name)
+        bucket = client.get_bucket(self.gcs_bucket_name)
+
+        blob_names_in_remote_directory = client.list_blobs(self.gcs_bucket_name, prefix=remote_directory, delimiter=None)
+        blob_file_pairs = [(blob, local_directory + blob.name.removeprefix(remote_directory)) for blob in blob_names_in_remote_directory]
+        for pair in blob_file_pairs:
+            print(pair[0].name, pair[1])
+        transfer_manager.download_many(blob_file_pairs, skip_if_exists=False)
+        #Todo: test if download is successful
+
     def store_flow_data(self, data: bytes, filename: str) -> str:
         from metaflow import current
 
@@ -68,3 +96,19 @@ class ArtifactStore:
         self.fetch(remote_path=path, local_path=path)
 
         return path
+
+    def fetch_all_flow_data_from_directory(self, flow_name: str, run_id: str, flow_subdir: str, local_dir: str) -> str:
+        storage_dir = self._get_flow_storage_directory(
+            flow_name=flow_name, run_id=run_id, remote_subdir=flow_subdir
+        )
+
+        self.fetch_directory(remote_directory=storage_dir, local_directory=local_dir)
+
+        return local_dir
+
+if __name__ == '__main__':
+    artifactStore: ArtifactStore = ArtifactStore("moz-fx-mlops-inference-nonprod", "mf-models-test1")
+    artifactStore.fetch_all_flow_data_from_directory("CodeAutocompletionFlow", "107", "trained/checkpoint-2", "./models/checkpoint-500")
+    #artifact_store = ArtifactStore(GCS_PROJECT_NAME, GCS_BUCKET_NAME)
+    #artifact_store.fetch_directory("copilot-demo/starcoderbase-1b", base_model_path)
+    #artifact_store.fetch_directory("copilot-demo/checkpoint-500", adapted_model_path)
