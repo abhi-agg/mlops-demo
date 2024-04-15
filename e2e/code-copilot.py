f711047c43ceb5ed931d769385896d014fc1367a
Abhishek Aggarwal <aaggarwal@mozilla.com>
2024-04-15 10:19:48 +0200

    Added code to modify checkpoint model config file
     - To point to a locally checkedout base model instead of
       model id on huggingface platform

--- e2e/code-copilot.py
+++ e2e/code-copilot.py
@@ -1,4 +1,6 @@
 import contextlib
+import os
+import json
 import torch
 from fastapi import FastAPI, Request
 from typing import Dict
@@ -15,6 +17,11 @@ from peft import (
 
 app = FastAPI()
 
+MODELS_LOCAL_DIRECTORY = "./models"
+BASE_MODEL_NAME = "starcoderbase-1b"
+ADAPTED_MODEL_NAME = "checkpoint-500"
+ADAPTED_MODEL_CONFIG_FILE_NAME = "adapter_config.json"
+ADAPTED_MODEL_CONFIG_KEY_TO_MODIFY = "base_model_name_or_path"
 
 class CopilotRequest(BaseModel):
     inputs: str = Field(description="The text of the prompt to use")
@@ -25,20 +32,33 @@ class CopilotRequest(BaseModel):
 @serve.ingress(app)
 class MozPilot:
     def __init__(self, args: Dict[str, str]) -> None:
-        self.tokenizer = AutoTokenizer.from_pretrained("../../starcoderbase-1b")
-        checkpoint_path = "../../checkpoint-500"
+        base_model_directory = os.path.join(MODELS_LOCAL_DIRECTORY, BASE_MODEL_NAME)
+        adapted_model_directory = os.path.join(MODELS_LOCAL_DIRECTORY, ADAPTED_MODEL_NAME)
+        adapted_model_config_file_path = os.path.join(adapted_model_directory, ADAPTED_MODEL_CONFIG_FILE_NAME)
+        self._modify_adapted_model_config(adapted_model_config_file_path, (ADAPTED_MODEL_CONFIG_KEY_TO_MODIFY, base_model_directory))
+
+        self.tokenizer = AutoTokenizer.from_pretrained(base_model_directory)
         model = AutoModelForCausalLM.from_pretrained(
-            checkpoint_path,
-            quantization_config=None, 
+            adapted_model_directory,
+            quantization_config=None,
             device_map=None,
             torch_dtype=torch.bfloat16,
         )
-        self.model = PeftModel.from_pretrained(model, checkpoint_path, adapter_name="copilot")
+        self.model = PeftModel.from_pretrained(model, adapted_model_directory, adapter_name="copilot")
         if not hasattr(self.model, "hf_device_map"):
             self.model.cuda()
 
         self.model.add_weighted_adapter(["copilot"], [0.8], "code_buddy")
         self.model.set_adapter("code_buddy")
+
+    def _modify_adapted_model_config(self, adapted_model_config_file_path: str, config_key_value_pair: tuple[str,str]):
+        """Modify the adapted model json config file to update the key value pair"""
+        with open(adapted_model_config_file_path, 'r+') as json_config_file:
+            dict_formatted_data = json.load(json_config_file)
+            dict_formatted_data[config_key_value_pair[0]] = config_key_value_pair[1]
+            json_config_file.seek(0)
+            json.dump(dict_formatted_data, json_config_file, indent=2)
+            json_config_file.truncate()
 
     def get_code_completion(self, prompt, disable=False):
         context = contextlib.nullcontext
